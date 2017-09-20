0e3a40bfd8b0d34d04bb621734267aa525a71960
Carlos Barragan <starttheshooting@hotmail.com>
2017-09-19 21:58:58 -0500

    First Lesson basic Python fundamentals

---
+++ lesson1/firstProgram.py
@@ -0,0 +1,57 @@
+
+say_hello = "Hello World"
+
+#print(say_hello)
+
+#print("\nHello Carlos B")
+
+#FUNCTIONS AND FORMAT STRING
+
+# program that prints that meme song
+# def my_function(line):
+#     print (line)
+#     print ('Hello Darkness my Old Friend')
+#     print ('It is nice to talk to you again')
+#     function_inception()
+#
+#
+# def function_inception():
+#     print ('Esta string se encuentra dentro de otra funcion')
+#
+#
+# my_function(say_hello)
+
+# def get_result():
+#     return "El resultado de la suma de {} + {} es igual a {}"
+#
+# def get_suma(x,y):
+#     suma = x + y
+#     return get_result().format(x, y, suma)
+#
+# def main_suma():
+#     print(get_suma(2,5))
+#     a = int(input("Enter a interger: "))
+#     b = int(input("Enter another interger: "))
+#     print(get_suma(a,b))
+#
+# main_suma()
+
+#IF ELSE
+
+# def if_else_function():
+#
+#     numero1 = 4
+#     numero2 = 4
+#
+#     if numero1 > numero2:
+#         print '{} es mayor que {}'.format(numero1, numero2)
+#     elif numero1 < numero2:
+#         print '{} es menor que {}'.format(numero1, numero2)
+#     elif numero1 == numero2:
+#         print 'son iguales'.format(numero1, numero2)
+#     else:
+#         print ('error')
+#
+# if_else_function()
+
+#TAREA UN REMINDER DE CADA 10 SEGUNDOS ABRIENDO UN LINK DE YOUTUBE CON TIME, WEB EXPLORER
