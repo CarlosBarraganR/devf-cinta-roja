0a396e367975f3bda90f981c1088de62e9ba19cb
Carlos Barragan <starttheshooting@hotmail.com>
2017-09-25 09:34:28 -0500

    Update euclidiana to vector with n-size

--- euclidiana/euclidiana.py
+++ euclidiana/euclidiana.py
@@ -10,13 +10,15 @@ point_1 = map(int, point_1)
 point_2 = map(int, point_2)
 vectors = [point_1, point_2]
 
-print  vectors
 
-def calculate_vector(p1, p2):
+def calculate_vector(p1, p2, result):
     if len(p1) != len(p2):
         print "Tamanhoos de puntos son diferentes"
     else:
-        result = math.sqrt(math.pow(p2[0] - p1[0], 2) + math.pow(p2[1] - p1[1], 2))
-        print "La distancia euclidiana es: " + str(result)
+        for i in range(len(p1)):
+            result = result + math.pow(p2[i]-p1[i], 2)
+        print "La distancia euclidiana es: " + str(math.sqrt(result))
 
-calculate_vector(point_1,point_2)
+
+total = 0
+calculate_vector(point_1, point_2, total)
